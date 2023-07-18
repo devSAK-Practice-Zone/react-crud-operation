3526db406026c07ddb764b864dc7ddcbf40468f4
Sabir Ahammed Khan <54625791+devSAK@users.noreply.github.com>
2023-07-18 12:26:41 +0530

    Adding Delete operation.

--- src/Components/Read.js
+++ src/Components/Read.js
@@ -14,13 +14,22 @@ const Read = () => {
   useEffect(() => {
     getData();
   }, []);
+
+  function handleDelete(id) {
+    axios
+      .delete(`https://642d2881bf8cbecdb4feac9c.mockapi.io/demoData/${id}`)
+      .then(() => {
+        getData();
+      });
+  }
+  
   return (
     <>
       <h2>Read Operation</h2>
       <table className="table">
         <thead>
           <tr>
-            <th scope="col">#</th>
+            <th scope="col">Id</th>
             <th scope="col">Name</th>
             <th scope="col">Email</th>
             <th scope="col">Edit</th>
@@ -45,7 +54,8 @@ const Read = () => {
                   <td>
                     <button
                       type="button"
-                      className="btn btn-danger">
+                      className="btn btn-danger"
+                      onClick={() => handleDelete(eachData.id)}>
                       Delete
                     </button>
                   </td>
